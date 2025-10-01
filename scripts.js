b9b64d703e49372623649b1c36e6cf5a0b7b8221
Matt H <227414719+waker-btn@users.noreply.github.com>
2025-10-01 13:34:00 +0100

    Initial JS

--- scripts.js
+++ scripts.js
@@ -1,31 +1,49 @@
+let searchResults;
+let parsedResults;
+
 function initialisePage() {
-
-    const searchSubmit = document.querySelector(".header__search__submit");
-    searchSubmit.addEventListener("click", commenceSearch);
-
+  const searchSubmit = document.querySelector(".header__search__submit");
+  searchSubmit.addEventListener("click", commenceSearch);
 }
 
-function commenceSearch(e){
+async function commenceSearch(e) {
+  e.preventDefault();
 
-    let searchQuery = document.querySelector(".header__search__text-query").value;
+  let searchQuery = document.querySelector(".header__search__text-query").value;
 
-    getResultsFromAPI(searchQuery);
+  searchResults = await getResultsFromAPI(searchQuery);
 
+  sortResults(searchResults);
+  displayResults(parsedResults);
 }
 
-async function getResultsFromAPI(searchQuery){
+async function getResultsFromAPI(searchQuery) {
+  let query = encodeURIComponent(searchQuery);
 
-        try {
-        const response = await fetch( + );
-        if (!response.ok) {
-            throw new Error("Network not OK");
-        }
-        return await response.json();
-    } catch (error) {
-        console.error("Failed to fetch Pokémon list:", error);
-        throw error;
+  let fetchAddress = "https://images-api.nasa.gov/search?q=" + query;
+
+  try {
+    const response = await fetch(fetchAddress);
+    if (!response.ok) {
+      throw new Error("Network not OK");
     }
-
-
+    return await response.json();
+  } catch (error) {
+    console.error("Failed to fetch collection:", error);
+    throw error;
+  }
 }
 
+function displayResults() {}
+
+function sortResults(results) {
+  let working = JSON.parse(results);
+  let items = working.collection.items;
+  let sorted;
+
+  for (const result in items) {
+    
+  }
+}
+
+initialisePage();
